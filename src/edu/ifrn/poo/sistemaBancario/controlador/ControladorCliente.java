c7cfe6b463e84c72ff9500e0e58f55ea61ed26fa
Cristiane Viana <Cristiane Viana@Cristiane>
2014-08-04 21:32:08 -0300

    Algumas atualizações..

--- src/edu/ifrn/poo/sistemaBancario/controlador/ControladorCliente.java
+++ src/edu/ifrn/poo/sistemaBancario/controlador/ControladorCliente.java
@@ -3,17 +3,30 @@ package edu.ifrn.poo.sistemaBancario.controlador;
 import edu.ifrn.poo.sistemaBancario.dao.ClienteDao;
 import edu.ifrn.poo.sistemaBancario.dominio.Cliente;
 import java.sql.SQLException;
+import javax.swing.table.DefaultTableModel;
 
 public class ControladorCliente {
     public void cadastrarCliente(Cliente c) throws ClassNotFoundException, SQLException {
         ClienteDao cliente_dao = new ClienteDao();
            
         cliente_dao.inserir(c);
-                
     }
-    public int getIdByNome(String nome) throws ClassNotFoundException, SQLException {
+       
+    public void verCliente(Cliente c) throws ClassNotFoundException, SQLException {
         ClienteDao cliente_dao = new ClienteDao();
-        
-        return cliente_dao.getIdByNome(nome);
+
+        cliente_dao.listarCliente();
     }
+    
+    public DefaultTableModel listarCliente() throws ClassNotFoundException, SQLException {
+        DefaultTableModel dft = new DefaultTableModel();
+        ClienteDao dao = new ClienteDao();
+        
+        dft.addColumn("Cliente ", dao.listarCliente());
+//        dft.addColumn("Data de Vencimento", dao.listarDataVenc());
+//        dft.addColumn("Categoria", dao.listarCategoria());
+ 
+        return dft;
+    }
+   
 }
--- src/edu/ifrn/poo/sistemaBancario/dao/ContaDao.java
+++ src/edu/ifrn/poo/sistemaBancario/dao/ContaDao.java
@@ -3,6 +3,7 @@ package edu.ifrn.poo.sistemaBancario.dao;
 import edu.ifrn.poo.sistemaBancario.dominio.Conta;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -13,7 +14,7 @@ public class ContaDao {
         Connection conn = ConnectionFactory.getConnection();        
         //construir o comando SQL
    
-        String sql = "INSERT INTO conta" +  "(idConta, numero, ativa, saldo) VALUES" + "(?,?,?,?)";
+        String sql = "INSERT INTO conta" +  "(numero, ativa, saldo) VALUES" + "(?,?,?)";
         PreparedStatement preparedStatement = conn.prepareStatement(sql);
         
 //        preparedStatement.setInt(1, 1);
@@ -24,8 +25,49 @@ public class ContaDao {
         //executar e validar o comando SQL.
         preparedStatement.executeUpdate();
     }
-    
-    public int getIdByNumero(int numero) {
-        throw new UnsupportedOperationException("Not yet implemented");
+
+    public Integer[] listarConta() throws ClassNotFoundException, SQLException {
+        //Estabelecer a conexão
+        Connection conn = ConnectionFactory.getConnection();
+        ResultSet rs;
+        Integer[] res = new Integer[this.quantidadeContas()];
+        int i = 0;
+        
+        //construir o comando SQL
+        String sql = "SELECT numero FROM Conta ORDER BY numero ASC";
+        PreparedStatement stm = conn.prepareStatement(sql);
+        
+        //executar e validar o comando SQL.
+        rs = stm.executeQuery();
+        
+        //Converter ResultSet em String        
+        while(rs.next()== true) {
+            res[i] = rs.getInt("numero");                       
+            i++;
+        }
+                
+        return res;
+                     
     }
+    
+    
+private int quantidadeContas() throws ClassNotFoundException, SQLException {
+        //Estabelecer a conexão
+        Connection conn = ConnectionFactory.getConnection();
+        ResultSet rs;        
+        int quantidade = 0;
+        
+        //construir o comando SQL
+        String sql = "SELECT COUNT(*) FROM conta";
+        PreparedStatement stm = conn.prepareStatement(sql);
+        
+        //executar e validar o comando SQL.
+        rs = stm.executeQuery();
+        
+        rs.next();        
+        quantidade = rs.getInt(1);
+                
+        return quantidade;
+    }
+
 }
